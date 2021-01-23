cefa51e102062d71b356636a15a3e17ad3ed806a
condor233 <e.hdelemos@gmail.com>
2021-01-23 17:27:58 -0300

    Adiciona modal

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ class Despesa {
         this.descricao = descricao
         this.valor = valor
     }
+
+    validarDados() {
+        for(let i in this) {
+            if(this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            }
+        }
+
+        return true
+    }
 }
 
 class Bd {
@@ -34,12 +44,12 @@ class Bd {
 let bd = new Bd();
 
 function cadastrarDespesa() {
-    document.getElementById('ano')
-    document.getElementById('mes')
-    document.getElementById('dia')
-    document.getElementById('tipo')
-    document.getElementById('descricao')
-    document.getElementById('valor')
+    let ano =   document.getElementById('ano')
+    let mes =  document.getElementById('mes')
+    let dia =  document.getElementById('dia')
+    let tipo =  document.getElementById('tipo')
+    let descricao =  document.getElementById('descricao')
+    let valor = document.getElementById('valor')
 
     let despesa = new Despesa (
         ano.value,
@@ -50,7 +60,26 @@ function cadastrarDespesa() {
         valor.value
     )
 
-    bd.gravar(despesa)
+    if(despesa.validarDados()) {
+       // bd.gravar(despesa)
+       document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+
+		$('#modalRegistraDespesa').modal('show') 
+    } else {
+        document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+        $('#modalRegistraDespesa').modal('show')
+
+    }
+
+   
 }
 
 
